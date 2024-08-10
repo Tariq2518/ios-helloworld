85862d32335b6a18d575255edc3381d163e3acd5
Tariq Hussain <tariqhussain@metricapp.co>
2024-08-10 18:53:14 +0500

    Initial Commit

---
+++ helloworld_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloworld_ios
+//
+//  Created by Tariq Hussain on 10/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloworld_ios/helloworld_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworld_iosApp.swift
+//  helloworld_ios
+//
+//  Created by Tariq Hussain on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct helloworld_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

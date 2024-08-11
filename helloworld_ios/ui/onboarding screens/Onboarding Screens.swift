f02ecfa321c1886325ef9ebcb51a1073b6aa0d7b
tariq2518 <tariq.hussain2518@gmail.com>
2024-08-11 18:07:34 +0500

    Onboarding Screens

---
+++ helloworld_ios/ui/onboarding screens/Onboarding Screens.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding Screens.swift
+//  helloworld_ios
+//
+//  Created by Tariq Hussain on 11/08/2024.
+//
+
+import SwiftUI
+
+struct Onboarding_Screens: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding_Screens()
+}
